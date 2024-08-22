cf811e9e24fac7200d9f6f01e572a3888687b53c
Artzy1401 <afifarsyi@gmail.com>
2024-08-22 08:17:56 +0700

    Adjust role model

--- internal/domain/models/roleModel.go
+++ internal/domain/models/roleModel.go
@@ -1,6 +1,14 @@
 package models
 
+import "time"
+
 type Role struct {
 	ID   uint   `gorm:"primaryKey" json:"id"`
 	Name string `gorm:"not null;unique" json:"name"`
+
+	Users []User `gorm:"foreignKey:RoleID"`
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time `gorm:"index" json:"deleted_at"`
 }
