b21dcd85e9c25773f4fa7959eb72a1709f8c5efb
Artzy1401 <afifarsyi@gmail.com>
2024-08-22 20:18:49 +0700

    Update jwt auth

--- internal/delivery/middleware/jwt_auth_midlleware.go
+++ internal/delivery/middleware/jwt_auth_midlleware.go
@@ -1,47 +1,55 @@
 package middleware
 
 import (
+	"errors"
 	"net/http"
-	"os"
 
+	"github.com/Arasy41/go-gin-quiz-api/internal/domain/models"
+	"github.com/Arasy41/go-gin-quiz-api/pkg/config"
+	"github.com/Arasy41/go-gin-quiz-api/pkg/jwt"
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v4"
-	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
-var jwtKey = []byte(os.Getenv("SECRET_KEY"))
+var jwtKey = []byte(config.AppConfig.SecretKey)
 
-func JWTAuthMiddleware() gin.HandlerFunc {
+func JwtAuthMiddleware(allowedRoles ...string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		authorizationHeader := c.GetHeader("Authorization")
-		if len(authorizationHeader) == 0 {
-			c.AbortWithStatus(http.StatusUnauthorized)
-			return
-		}
-
-		tokenString := authorizationHeader
-
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, jwt.ErrSignatureInvalid
-			}
-
-			return jwtKey, nil
-		})
-
+		err := jwt.TokenValid(c)
 		if err != nil {
-			c.AbortWithStatus(http.StatusUnauthorized)
+			c.String(http.StatusUnauthorized, err.Error())
+			c.Abort()
 			return
 		}
 
-		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-			id := uuid.MustParse(claims["id"].(string))
-			c.Set("id", id)
-			c.Set("username", claims["username"])
-			c.Set("role_id", claims["role_id"])
-			c.Next()
-		} else {
-			c.AbortWithStatus(http.StatusUnauthorized)
+		userId, err := jwt.ExtractTokenID(c)
+		if err != nil {
+			c.String(http.StatusUnauthorized, err.Error())
+			c.Abort()
+			return
 		}
+
+		var user models.User
+		db := c.MustGet("db").(*gorm.DB)
+		findUserErr := db.Preload("Role").Where("id = ?", userId).First(&user).Error
+		if findUserErr != nil {
+			c.String(http.StatusUnauthorized, findUserErr.Error())
+			c.Abort()
+			return
+		}
+
+		// Check if the user's role is allowed to access the route
+		for _, role := range allowedRoles {
+			if role == user.Role.Name || user.Role.Name == "admin" {
+				c.Set("user_id", user.ID)
+				c.Set("user_role", user.Role.Name)
+				c.Next()
+				return
+			}
+		}
+
+		roleError := errors.New("sorry, your role cannot access this route")
+		c.String(http.StatusForbidden, roleError.Error())
+		c.Abort()
 	}
 }
--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,50 +1,89 @@
 package jwt
 
 import (
-	"os"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/Arasy41/go-gin-quiz-api/pkg/config"
+	"github.com/Arasy41/go-gin-quiz-api/pkg/helper"
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 )
 
-var jwtKey = []byte(os.Getenv("SECRET_KEY"))
+var API_SECRET = config.AppConfig.SecretKey
 
-type Claims struct {
-	ID       uint
-	Username string
-	RoleID   uint
-	jwt.StandardClaims
-}
+func GenerateToken(user_id, role_id uint) (string, error) {
+	token_lifespan, err := strconv.Atoi(helper.Getenv("TOKEN_HOUR_LIFESPAN", "1"))
 
-func GenerateJWT(id uint, username string, roleID uint) (string, error) {
-	expirationTime := time.Now().Add(1 * time.Hour)
+	if err != nil {
+		return "", err
+	}
 
-	claims := &Claims{
-		ID:       id,
-		Username: username,
-		RoleID:   roleID,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: expirationTime.Unix(),
-		},
+	claims := jwt.MapClaims{
+		"authorized": true,
+		"user_id":    user_id,
+		"role":       role_id,
+		"exp":        time.Now().Add(time.Hour * time.Duration(token_lifespan)).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	return token.SignedString(jwtKey)
+	return token.SignedString([]byte(API_SECRET))
+
 }
 
-func ValidateToken(signedToken string) (*jwt.Token, error) {
-	token, err := jwt.ParseWithClaims(
-		signedToken,
-		&Claims{},
-		func(token *jwt.Token) (interface{}, error) {
-			return []byte(jwtKey), nil
-		},
-	)
-
+func TokenValid(c *gin.Context) error {
+	tokenString := ExtractToken(c)
+	_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(API_SECRET), nil
+	})
 	if err != nil {
-		return nil, err
+		return err
+	}
+	return nil
+}
+
+func ExtractToken(c *gin.Context) string {
+	token := c.Query("token")
+
+	if token != "" {
+		return token
 	}
 
-	return token, nil
+	bearerToken := c.Request.Header.Get("Authorization")
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		return strings.Split(bearerToken, " ")[1]
+	}
+
+	return ""
+}
+
+func ExtractTokenID(c *gin.Context) (uint, error) {
+
+	tokenString := ExtractToken(c)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(API_SECRET), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		uid, err := strconv.ParseUint(fmt.Sprintf("%.0f", claims["user_id"]), 10, 32)
+		if err != nil {
+			return 0, err
+		}
+		return uint(uid), nil
+	}
+
+	return 0, nil
 }
